bc37bae43058550cef2ec0558e17213d30595ae8
Hamid Bekamiri <hamid.bekam@gmail.com>
2024-07-02 14:17:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ def main():
     # Player selection
     st.header('Number of Players')
     col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        if st.button('1 Player'):
-            st.write('1 Player Selected')
-    with col2:
-        if st.button('2 Players'):
-            st.write('2 Players Selected')
-    with col3:
-        if st.button('3 Players'):
-            st.write('3 Players Selected')
-    with col4:
-        if st.button('4 Players'):
-            st.write('4 Players Selected')
+    st.button('Kick off')
+    # with col1:
+    #     if st.button('1 Player'):
+    #         st.write('1 Player Selected')
+    # with col2:
+    #     if st.button('2 Players'):
+    #         st.write('2 Players Selected')
+    # with col3:
+    #     if st.button('3 Players'):
+    #         st.write('3 Players Selected')
+    # with col4:
+    #     if st.button('4 Players'):
+    #         st.write('4 Players Selected')
 
     # Game mode selection
     st.header('Game Mode')
